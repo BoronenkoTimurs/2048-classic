17d6b455e522b11bf23dfbde90cefbe8de3f3216
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-12-11 11:40:09 +0200

    React + tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gameBackground w-max m-auto p-2 rounded-md mt-3">
+      {data.map((row, oneIndex) => {
+        return (
+          <div className="flex" key={oneIndex}>
+            {row.map((digit, index) => (
+              <Block num={digit} key={index} />
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
+const Block = ({ num }) => {
+  return <div>{num}</div>;
+};
 
 export default App;
